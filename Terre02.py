ae215f7776121bbfcf97cc391f47f0cd59cf2938
CorentinGar <garbaycorentin2@gmail.com>
2023-06-27 10:05:56 +0200

    27/06/23 10:05

--- Terre02.py
+++ Terre02.py
@@ -8,3 +8,11 @@
 
 
 
+import sys
+
+def diplayArgument():
+    arguments = sys.argv[1:]    
+    for argument in arguments:
+        print(argument)
+
+diplayArgument()
---
+++ Terre03.py
@@ -0,0 +1,6 @@
+
+
+import os
+
+term_program = os.environ.get('TERM_PROGRAM')
+print("Terminal utilisé :", term_program)
